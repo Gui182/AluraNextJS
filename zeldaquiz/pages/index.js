a2aa98eb0d4789d9c5dfdff1ff5f8cd18b65ddec
Guilherme <guilherme.augusto@linx.com.br>
2021-01-26 14:56:58 -0300

    Termino do segundo dia

--- zeldaquiz/pages/index.js
+++ zeldaquiz/pages/index.js
@@ -4,6 +4,10 @@ import Widget from '../src/components/Widget'
 import QuizBackground from '../src/components/QuizBackground'
 import Footer from '../src/components/Footer'
 import GitHubCorner from '../src/components/GitHubCorner'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+import Head from 'next/head'
+import QuizLogo from '../src/components/QuizLogo'
 
 const Title = styled.h1`
   font-size: 50px;
@@ -28,24 +32,47 @@ export const QuizContainer = styled.div`
 `
 
 export default function Home() {
+  const router = useRouter()
+  const [name, setName] = React.useState('')
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>AluraQuiz - Modelo Base</title>
+      </Head>
       <QuizContainer>
+        <QuizLogo />
         <Widget>
           <Widget.Header>
-            <h1>
-              Teste
-          </h1>
+            <h1>The Legend of Zelda</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>
-              teste @2
-          </p>
+            <form onSubmit={function (e) {
+              e.preventDefault()
+              router.push(`/quiz?name=${name}`)
+            }}>
+              <input
+                onChange={function (e) {
+                  setName(e.target.value)
+                }}
+                placeholder="Diz ai seu nome"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
+          </Widget.Content>
+        </Widget>
+
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/GuiLaTT/AluraNextJS"/>
+      <GitHubCorner projectUrl="https://github.com/GuiLaTT/AluraNextJS" />
     </QuizBackground>
   )
 }
